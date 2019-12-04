638bf2b5631cc448c1e700c1bc5c57d68cd3650b
Jeffrey A Mattson <jmattson@sei.cmu.edu>
2019-12-04 11:16:18 -0500

    iftag guestsettings quickfix

--- src/TopoMojo.vSphere/VimTransform.cs
+++ src/TopoMojo.vSphere/VimTransform.cs
@@ -266,6 +266,13 @@ namespace TopoMojo.vSphere
             {
                 foreach (var setting in template.GuestSettings)
                 {
+                    // TODO: rework this quick fix for injecting isolation specific settings
+                    if (setting.Key.StartsWith("iftag-") && !setting.Value.Contains(template.IsolationTag))
+                    {
+                        continue;
+                    }
+                    setting.Key = setting.Key.Replace("iftag-", "");
+
                     options.Add(new OptionValue { key = setting.Key, value = setting.Value });
                 }
             }
